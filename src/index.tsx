2c41ceb6b01c6ebb9de66df78291f4e6756a870f
kwy <scoworks.kwy@gmail.com>
2022-05-16 19:08:10 +0900

    frontend 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from "@apollo/client";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { client } from "./apollo";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./styles/styles.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
